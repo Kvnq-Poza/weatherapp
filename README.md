338a0fc9ab08da61a286c1b238c287b179005c26
Kvnq-Poza <114484337+Kvnq-Poza@users.noreply.github.com>
2022-10-07 11:43:26 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# weatherapp
+ A weather app created using python to pull request from the Open Weather API to give real-time weather information on any region
---
+++ weatherforecast/weather/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+import json
+import urllib.request
+
+# Create your views here.
+def index(request):
+    if request.method == 'POST':
+        city = request.POST['city']
+        res = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=6e98ccd872416c1137de4ecc096d118d').read()
+        json_data = json.loads(res)
+        data = {
+            'country_code': str(json_data['sys']['country']),
+            'coordinate': str(json_data['coord']['lon']) + ' ' +
+            str(json_data['coord']['lat']),
+            'temp':str(json_data['main']['temp'])+'k',
+            'pressure': str(json_data['main']['pressure']),
+            'humidity': str(json_data['main']['humidity']),
+        }
+
+    else:
+        city = ''
+        data = {}
+    return render(request, 'index.html', {'city': city, 'data': data})
